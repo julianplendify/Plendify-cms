0be98a6076628af4b032a3de43bac835973581a3
Kellycypher98 <Kelvinboateng94@outlook.com>
2025-11-03 09:42:04 +0100

    db

--- config/admin.ts
+++ config/admin.ts
@@ -10,9 +10,11 @@ export default ({ env }) => ({
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY'),
+  },
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
-  forceSecureCookie: false, // THIS IS THE KEY LINE
 });
